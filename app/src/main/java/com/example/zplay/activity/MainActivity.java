41d250356b08cf7d7c82b9a237a2e5dd15269099
zhuwujing <zwj@ldrobot.com>
2019-11-10 23:53:35 +0800

    帧动画和补间动画

--- app/src/main/java/com/example/zplay/activity/MainActivity.java
+++ app/src/main/java/com/example/zplay/activity/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.zplay.activity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -22,19 +23,35 @@ public class MainActivity extends AppCompatActivity {
     TextView animContent;
     @BindView(R.id.begin_anim_btn)
     Button beginAnimBtn;
+    @BindView(R.id.frame_anim_iv)
+    ImageView frameAnimIV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+//        AnimationDrawable animationDrawable = (AnimationDrawable) frameAnimIV.getBackground();
+//        animationDrawable.start();
+
+        AnimationDrawable animationDrawable = new AnimationDrawable();
+        for (int i = 1; i < 10; i++) {
+            int drawableResId = getResources().getIdentifier("now_playing_matrix_0" + i, "drawable", getPackageName());
+            animationDrawable.addFrame(getResources().getDrawable(drawableResId), 200);
+        }
+        frameAnimIV.setImageDrawable(animationDrawable);
+        animationDrawable.start();
     }
 
     @OnClick(R.id.begin_anim_btn)
     public void onClicked(View view) {
         switch (view.getId()) {
             case R.id.begin_anim_btn:
-                ZAnimationUtil.startAnimation(animContent, 0f, UIUtils.getScreenWith(this) - animContent.getPaint().measureText(animContent.getText().toString().trim()), 0f, 0f);
+//                ZAnimationUtil.startTranslateAnimation(animContent, 0f, UIUtils.getScreenWith(this) - animContent.getPaint().measureText(animContent.getText().toString().trim()), 0f, 0f);
+//                ZAnimationUtil.startTranslateAnimationByXML(this, animContent, R.anim.translate_animation);
+//                ZAnimationUtil.startAlphaAnimation(animContent, 1, 0);
+                ZAnimationUtil.atartAlphaAnimationByXML(animContent, R.anim.scale_animation);
                 break;
         }
 
--- zanimation/src/main/java/com/example/zanimation/ZAnimationUtil.java
+++ zanimation/src/main/java/com/example/zanimation/ZAnimationUtil.java
@@ -1,7 +1,13 @@
 package com.example.zanimation;
 
+import android.content.Context;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.view.animation.TranslateAnimation;
+
+import androidx.annotation.AnimRes;
 
 /**
  * 创建时间：2019/11/10  12:03
@@ -10,7 +16,7 @@ import android.view.animation.TranslateAnimation;
  */
 public class ZAnimationUtil {
     /**
-     * 平移动画
+     * java代码：平移动画
      *
      * @param target
      * @param fromXDelta
@@ -18,13 +24,42 @@ public class ZAnimationUtil {
      * @param fromYDelta
      * @param toYDelta
      */
-    public static void startAnimation(View target, float fromXDelta, float toXDelta, float fromYDelta, float toYDelta) {
+    public static void startTranslateAnimation(View target, float fromXDelta, float toXDelta, float fromYDelta, float toYDelta) {
         TranslateAnimation translateAnimation = new TranslateAnimation(fromXDelta, toXDelta, fromYDelta, toYDelta);
         translateAnimation.setDuration(3000);
-//        translateAnimation.setFillAfter(true);
-        translateAnimation.setFillBefore(true);
+        translateAnimation.setFillAfter(true);
         target.startAnimation(translateAnimation);
     }
 
+    /**
+     * xml：平移动画
+     *
+     * @param context
+     * @param target
+     * @param ResId
+     */
+    public static void startTranslateAnimationByXML(Context context, View target, @AnimRes int ResId) {
+        Animation animation = AnimationUtils.loadAnimation(context, ResId);
+        target.startAnimation(animation);
+    }
+
+    /**
+     * java代码：透明动画
+     *
+     * @param target
+     * @param fromAlpha
+     * @param toAlpha
+     */
+    public static void startAlphaAnimation(View target, int fromAlpha, int toAlpha) {
+        AlphaAnimation alphaAnimation = new AlphaAnimation(fromAlpha, toAlpha);
+        alphaAnimation.setDuration(3000);
+        target.startAnimation(alphaAnimation);
+    }
+
+    public static void atartAlphaAnimationByXML(View target, @AnimRes int resId) {
+        Animation animation = AnimationUtils.loadAnimation(target.getContext(), resId);
+        animation.setDuration(3000);
+        target.startAnimation(animation);
+    }
 
 }
